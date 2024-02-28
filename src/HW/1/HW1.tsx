044b153f3e60ab89471d93d985a88971cad557f0
arseniiOsovets <arsenasavets@gmail.com>
2024-02-28 15:59:58 +0300

    1_Commit

--- src/HW/1/HW1.tsx
+++ src/HW/1/HW1.tsx
@@ -34,7 +34,8 @@ export const HW1 = () => {
 
 	return (
 		<div id={'hw01'}>
-			{/*<UserList users={'НАЗВАНИЕ МАССИВА, КОТОРЫЙ МЫ ПЕРЕДАЕМ'} />*/}
+			<UserList  user={users} />
 		</div>
+		
 	);
 };
